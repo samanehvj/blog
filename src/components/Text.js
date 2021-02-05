6627e7761c77f3a3916d30a22df7fca704e42e78
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-02-05 10:28:40 -0800

    Create Text.js

---
+++ src/components/Text.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import COLORS from '../helpers/colors';
+
+
+const Text = styled.Text`
+  font-size: 25;
+  font-weight: 300;
+  margin-top: 20;
+  margin-left: 30;
+  color: ${COLORS.textColor};
+`;
+
+export default Text;
