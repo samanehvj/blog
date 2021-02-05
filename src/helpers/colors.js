b2192b430a04c61a20eff0755f71a0492569694c
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-02-05 10:27:13 -0800

    Create color.js

---
+++ src/helpers/colors.js
@@ -0,0 +1,8 @@
+export default COLORS = {
+  buttonBackgroundColor: '#000080',
+  buttonTextColor: '#ffffff',
+  textColor: 'navy',
+  primaryColor: 'black',
+  secondayColor: '#ffffff',
+  borderColor: '#000080',
+};
