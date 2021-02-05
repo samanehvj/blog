5e4416e5e1970e66baa35d526eed717927b7bbe7
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-02-05 10:28:15 -0800

    Create celebrityScreen.js css

--- src/screens/celebrityScreen.js
+++ src/screens/celebrityScreen.js
@@ -1,39 +1,47 @@
 import React from 'react';
+import Text from '../components/Text';
+import Button from '../components/Button';
+
 import {
   SafeAreaView,
-  Text,
   TextInput,
   View,
   ScrollView,
   Image,
   FlatList,
   StyleSheet,
-  Button,
 } from 'react-native';
 
 const styles = StyleSheet.create({
   sm: {
-    width: 400,
+    width: 450,
     height: 400,
+    padding: 60, 
+    marginBottom: 15, 
   },
   md: {
     width: 100,
     height: 100,
   },
+  container: {
+    backgroundColor: 'lightpink',
+  },
 });
+
 
 const CelebrityScreen = ({navigation, route}) => {
   return (
-    <SafeAreaView>
-      <Text style={{padding: 40,margin:30,color:'red'}}>This is {route.params.name}'s profile</Text>
+    <View style={styles.container}>
       <Image
         style={styles.sm}
         source={{
           uri: `${route.params.img}`,
         }}
       />
-    </SafeAreaView>
+     <Text>This is {route.params.name}'s profile</Text>
+     </View>
   );
 };
+
 
 export default CelebrityScreen;
