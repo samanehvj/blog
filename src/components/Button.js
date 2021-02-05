79fc4312495cb0951dd0d9ec1991b02ef822938b
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-02-05 10:29:21 -0800

    Create Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.Button`
+  font-size: 25;
+  font-weight: 300;
+  margin-top: 20;
+  margin-left: 30;
+  border: red;
+`;
+
+export default Button;
